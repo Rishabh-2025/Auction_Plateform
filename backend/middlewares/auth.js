2e77d7ba314c2c7afa09acd2d1dd056c4cc6e236
Rishabh <121664209+Rishabh-2025@users.noreply.github.com>
2025-05-22 13:51:22 +0530

    Update original auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,19 +5,14 @@ import { catchAsyncErrors } from "./catchAsyncErrors.js";
 
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const token = req.session.token;
-console.log(token);
-  if (!token) {
-    return next(new ErrorHandler("User not authenticated.", 401));
-  }
 
+  const token = req.cookies.token;
+
+  if (!token) {
+    return next(new ErrorHandler("User not authenticated.", 400));
+  }
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
-
-  if (!req.user) {
-    return next(new ErrorHandler("User not found.", 404));
-  }
-
   next();
 });
 
